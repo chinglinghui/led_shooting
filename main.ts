ffbff53116d15ef6b4d5ddf9c275e0c75b2fb370
chinglinghui <91246953+chinglinghui@users.noreply.github.com>
2021-12-30 16:40:32 +0800

    射擊遊戲，有兩台敵機，有背景音樂

--- main.ts
+++ main.ts
@@ -18,6 +18,8 @@ game.setScore(0)
 主角 = game.createSprite(2, 4)
 basic.pause(500)
 let 飛機 = game.createSprite(0, 0)
+basic.pause(500)
+let 飛機2 = game.createSprite(4, 0)
 basic.forever(function () {
     basic.pause(500)
     飛機.change(LedSpriteProperty.X, 1)
@@ -28,14 +30,40 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
+    basic.pause(500)
+    飛機2.change(LedSpriteProperty.X, -1)
+    if (飛機2.get(LedSpriteProperty.X) == 0) {
+        basic.pause(100)
+        飛機2.set(LedSpriteProperty.X, 4)
+        飛機2.change(LedSpriteProperty.Y, 1)
+    }
+})
+basic.forever(function () {
+    music.stopAllSounds()
+    music.playMelody("A G E G D C5 E F ", 120)
+})
+basic.forever(function () {
     if (飛機.isTouching(主角)) {
         game.gameOver()
     }
     if (子彈) {
         if (子彈.isTouching(飛機)) {
-            game.addScore(1)
             飛機.set(LedSpriteProperty.X, 0)
             飛機.set(LedSpriteProperty.Y, 0)
+            game.addScore(1)
+            子彈.delete()
+        }
+    }
+})
+basic.forever(function () {
+    if (飛機2.isTouching(主角)) {
+        game.gameOver()
+    }
+    if (子彈) {
+        if (子彈.isTouching(飛機2)) {
+            飛機2.set(LedSpriteProperty.X, 4)
+            飛機2.set(LedSpriteProperty.Y, 0)
+            game.addScore(1)
             子彈.delete()
         }
     }
