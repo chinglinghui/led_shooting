73db1ea53b3bdf496cd58009d380a650a9d8c7cb
chinglinghui <91246953+chinglinghui@users.noreply.github.com>
2022-01-06 16:52:27 +0800

    解決"不斷累積發射"的問題，使用"在迴圈中判斷A+B"的方法。更改音效。

--- main.ts
+++ main.ts
@@ -1,26 +1,19 @@
 input.onButtonPressed(Button.A, function () {
     主角.change(LedSpriteProperty.X, -1)
 })
-input.onButtonPressed(Button.AB, function () {
-    子彈 = game.createSprite(主角.get(LedSpriteProperty.X), 主角.get(LedSpriteProperty.Y))
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    for (let index = 0; index < 4; index++) {
-        子彈.change(LedSpriteProperty.Y, -1)
-        basic.pause(200)
-    }
-    子彈.delete()
-})
 input.onButtonPressed(Button.B, function () {
     主角.change(LedSpriteProperty.X, 1)
 })
 let 子彈: game.LedSprite = null
 let 主角: game.LedSprite = null
+music.setVolume(90)
 game.setScore(0)
 主角 = game.createSprite(2, 4)
 basic.pause(500)
 let 飛機 = game.createSprite(0, 0)
 basic.pause(500)
 let 飛機2 = game.createSprite(4, 0)
+let 狀態 = 0
 basic.forever(function () {
     basic.pause(500)
     飛機.change(LedSpriteProperty.X, 1)
@@ -40,32 +33,45 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    if (飛機.isTouching(主角)) {
-        game.gameOver()
-        music.stopAllSounds()
-        music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+    while (input.buttonIsPressed(Button.AB)) {
+        if (狀態 == 0) {
+            soundExpression.slide.play()
+            狀態 = 1
+            子彈 = game.createSprite(主角.get(LedSpriteProperty.X), 主角.get(LedSpriteProperty.Y))
+            for (let index = 0; index < 4; index++) {
+                子彈.change(LedSpriteProperty.Y, -1)
+                basic.pause(100)
+            }
+            子彈.delete()
+            狀態 = 0
+        }
     }
+})
+basic.forever(function () {
     if (子彈) {
+        if (飛機.isTouching(主角)) {
+            game.gameOver()
+        }
         if (子彈.isTouching(飛機)) {
             飛機.set(LedSpriteProperty.X, 0)
             飛機.set(LedSpriteProperty.Y, 0)
             game.addScore(1)
             子彈.delete()
+            狀態 = 0
         }
     }
 })
 basic.forever(function () {
-    if (飛機2.isTouching(主角)) {
-        music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
-        game.gameOver()
-        music.stopAllSounds()
-    }
     if (子彈) {
+        if (飛機2.isTouching(主角)) {
+            game.gameOver()
+        }
         if (子彈.isTouching(飛機2)) {
             飛機2.set(LedSpriteProperty.X, 4)
             飛機2.set(LedSpriteProperty.Y, 0)
             game.addScore(1)
             子彈.delete()
+            狀態 = 0
         }
     }
 })
